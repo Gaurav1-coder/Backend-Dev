0d4bdbf89a1e2dc472854bde10e7cfdb8b63c01d
Gaurav1-coder <katiyargaurav358@email.com>
2026-01-20 14:18:15 +0530

    [Gaurav] Added Express project work

---
+++ ExpressProject/data.js
@@ -0,0 +1,28 @@
+const userData = [
+  {
+    id: 1,
+    name: "Amit Kumar",
+    age: 30,
+    email: "sdjbgjksg@sddgfnfk"
+  },
+  {
+    id: 2,
+    name: "Amit",
+    age: 25,
+    email: "amitkumar@kfj"
+  },
+  {
+    id: 3,
+    name: "sk",
+    age: 28,
+    email: "skdjgk@fhdg"
+  },
+  {
+    id: 4,
+    name: "vasudhaera",
+    age: 22,
+    email: "vasudhaera@dhgf"
+  }
+];
+
+module.exports = { userData };
